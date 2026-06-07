f92d16d0e8b9852c09773f057440b952e487c082
FlyTogether21 <flytogether29@gmail.com>
2026-06-07 19:06:10 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter a");
+    scanf("%",&a);
+    printf("enter b");
+    scanf("%",&b);
+    c=a+b;
+    printf(c)
+    getch();
+}
